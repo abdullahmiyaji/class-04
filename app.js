7b9fc6e5fb2e5a04c57d22f5d57b5fe2defcc106
abdullah-011 <you@example.com>
2023-06-14 22:55:08 +0300

    update

--- app.js
+++ app.js
@@ -56,6 +56,135 @@ const triangleArea = (base,height) => `Triangle Area is = ${(base * height) /2}`
 
 console.log(triangleArea(10,16));
 
+// 3.GPA, CGPA, GRADE function for result publishing
+// Answer
+
+const studentsOb = [
+      {     
+            id: 1,
+            name : "Ashikka",
+            age: 20,
+            location: "Mirpur",
+            isMarried: false,
+            gender: "Female",
+            movie: ["KGF","pathan", "Puspa" ],
+            result: {
+                  ban: 60,
+                  eng: 80,
+                  math: 97,
+                  science: 70,
+                  socialScience:47,
+                  reli: 55,
+            }
+      },
+      {     
+            id: 2,
+            name : "prokash",
+            age: 33,
+            location: "Mirpur",
+            isMarried: true,
+            gender: "male",
+            movie: ["Avatar","doctor steven strange", "Titanic" ],
+            result: {
+                  ban: 60,
+                  eng: 80,
+                  math: 17,
+                  science: 70,
+                  socialScience:19,
+                  reli: 55,
+            }
+      },
+      {     
+            id: 3,
+            name : "Mahbub",
+            age: 70,
+            location: "uttara",
+            isMarried: false,
+            gender: "male",
+            movie: ["Tokai","pathan", "Puspa" ],
+            result: {
+                  ban: 60,
+                  eng: 80,
+                  math: 97,
+                  science: 70,
+                  socialScience:47,
+                  reli: 55,
+            }
+      },
+      {
+            id: 4,
+            name : "ruma",
+            age: 20,
+            location: "Mirpur",
+            isMarried: false,
+            gender: "Female",
+            movie: ["KGF","pathan", "Puspa" ],
+            result: {
+                  ban: 60,
+                  eng: 80,
+                  math: 97,
+                  science: 70,
+                  socialScience:47,
+                  reli: 55,
+            }
+      },
+      
+]
+
+
+const getResult = (mark) => {
+      
+      let grade ;
+      let gpa ;
+      
+      if(mark >= 0 && mark < 33){
+          grade = "F"
+          gpa  = 0
+      
+      } else if(mark >= 33 && mark < 40){
+          grade = "D"
+          gpa  = 1
+      
+      } else if ( mark >= 40 && mark < 50){
+          grade = "C"
+          gpa  = 2
+      
+      } else if ( mark >= 50 && mark < 60){
+          grade = "b"
+          gpa  = 3
+      
+      } else if ( mark >= 60 && mark < 70){
+          grade = "A-"
+          gpa  = 3.5
+      
+      } else if ( mark >= 70 && mark < 80){
+          grade = "A"
+          gpa  = 4
+      
+      }else if (mark >= 80 && mark <= 100){
+          grade = "A+"
+          gpa  = 5
+      
+      } else{
+          grade = `Invalid`
+          gpa = `Invalid`
+      }
+      
+      return {
+            grade: grade,
+            gpa: gpa,
+      }
+      }
+      
+      console.log(getResult(77).gpa)
+      console.log(getResult(77).grade)
+      
+      
+      studentsOb.map((item) => {
+            
+                  console.log(` Student Name : ${item.name} Bangla Marks = ${ item.result.ban} GPA = ${getResult(item.result.ban).gpa}`)
+            })
+            
 // 4.Create an age calculator function
 
 let currentYear = 2023;
